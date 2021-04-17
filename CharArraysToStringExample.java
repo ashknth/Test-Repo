113b505685802f294a508e654836d00fbf4407a1
Pooja Bajagain <bajagainpooja098@gmail.com>
2021-04-17 11:11:48 +0545

    pooja commit 2

---
+++ CharArraysToStringExample.java
@@ -0,0 +1,9 @@
+public class Charac  
+{  
+public static void main(String args[])  
+{    
+char[] ch = {'w', 'e', 'l', 'c', 'o', 'm', 'e', ' ' , 't', 'o', ' ', 'J', 'a', 'v', 'a', 't', 'p', 'o', 'i', 'n', 't'};    
+String string = new String(ch);  
+System.out.println(string);  
+}  
+}
